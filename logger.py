a982e2a56dd4c6b39a7b7022e43509031bb0057d
Marcos Ribeiro <marcosribeiro.br@gmail.com>
2015-02-24 09:55:23 -0300

    Comments added.

--- logger.py
+++ logger.py
@@ -6,38 +6,56 @@ import subprocess
 import serial
 
 def get_temp():
+        # le o valor do A/D
         output = subprocess.check_output("cat /sys/bus/iio/devices/iio:device0/in_voltage0_raw", shell=True)
+        # Converte para tensao
         voltage = (float(output)*1.8)/4096.0
+        # Converte para temperatura
         temperature = (voltage*100)
+        # Formata a casas decimais
         temperature = "{:3.4f}".format(temperature)
+        # Converte para string e retorna a temperatura
         return str(temperature)
 
 def check_serial(sample):
+        # Le a serial
         byte = ser.read()
         if byte == "1":
+                # Apresenta o ultimo amostra
                 ser.write("\n\rUltimo valor:\n");
                 ser.write("\r"+sample+"\n")
         elif byte == "2":
+                # Apresenta todas amostras armazenadas no log
                 ser.write("\n\rOs valores armazenados sao:"+"\n")
+                # Aponta para o comeco do arquivo
                 file.seek(0, 0)
+                # Faz varredura das linhas imprimindo
                 for line in file:
                         ser.write("\r"+line)
         elif byte == "3":
+                # Sair do script
                 ser.write("\n\rSaindo..."+"\n")
                 file.close()
                 exit()
         elif len(byte) != 0:
+                # Verifica se chegou comando invalido
                 ser.write("\n\rComando invalido!"+"\n")
 
 def get_date():
+        # Verifica a data atual
         return str(datetime.datetime.now())
 
 def store_sample(sample):
+        # Aponta para o final do arquivo
         file.seek(0, 2)
+        # Armazena a amostra
         file.write(sample + '\n')
-
+        
+        #main
 if __name__ == "__main__":
+        # Inicializa a serial 115200
         ser = serial.Serial('/dev/ttyO4', 115200, timeout=0.01)
+        # Abre arquivo de log
         file = open('log.txt', 'w+')
         while True:
                 temp = get_temp()
