0aed89b7c9f231a9bb4c81d21b84c162889c13a7
Marcos Ribeiro <marcosribeiro.br@gmail.com>
2015-02-27 08:40:04 -0300

    until remove.
    selection=
    until [ "$selection" = "nao" ]; do
            read selection
            case $selection in
                    "sim" )
                            echo "Entre com o data (YYYY-MM-DD):"
                            read data
                            # Armazena a data informada em date (-s seta)
                            date -s $data
                            echo "Entre com a hora (HH:MM:SS):"
                            read hora
                            # Armazena a hora informada em data
                            date -s $hora
                            break
                            ;;
                    "nao" ) break ;;
                    * ) echo "Entrada invalida."
            esac
    done

--- config.sh
+++ config.sh
@@ -15,24 +15,21 @@ echo BB-UART4 > /sys/devices/bone_capemgr.*/slots
 echo "Perifericos configurados..."
 echo ""
 echo "Deseja configurar data e hora? (sim/nao)"
-selection=
-until [ "$selection" = "nao" ]; do
-        read selection
-        case $selection in
-                "sim" )
-                        echo "Entre com o data (YYYY-MM-DD):"
-                        read data
-                        # Armazena a data informada em date (-s seta)
-                        date -s $data
-                        echo "Entre com a hora (HH:MM:SS):"
-                        read hora
-                        # Armazena a hora informada em data
-                        date -s $hora
-                        break
-                        ;;
-                "nao" ) break ;;
-                * ) echo "Entrada invalida."
-        esac
+case $selection in
+        "sim" )
+                echo "Entre com o data (YYYY-MM-DD):"
+                read data
+                # Armazena a data informada em date (-s seta)
+                date -s $data
+                echo "Entre com a hora (HH:MM:SS):"
+                read hora
+                # Armazena a hora informada em data
+                date -s $hora
+                break
+                ;;
+        "nao" ) break ;;
+        * ) echo "Entrada invalida."
+esac
 done
 echo ""
 echo "Chamando script Python..."
